915c4156d3e27b10c930bef950b7a98b77fc7930
MMyeong <ilikerunning@naver.com>
2024-10-16 15:55:48 +0900

    chore: 환경변수명 수정

--- supabase/functions/client.ts
+++ supabase/functions/client.ts
@@ -1,9 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.44.3";
 
-const supabaseProjectURL = Deno.env.get("SUPA_PROJECT_URL")!;
-const supabaseServiceRoleKey = Deno.env.get("SUPA_SERVICE_ROLE_KEY")!;
-
 export const supabase = createClient(
-  supabaseProjectURL,
-  supabaseServiceRoleKey,
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );
