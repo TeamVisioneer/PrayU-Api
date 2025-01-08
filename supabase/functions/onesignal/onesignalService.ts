9341e0ea2e3b2541662c069a5beee6216006c203
MMyeong <ilikerunning@naver.com>
2025-01-09 00:54:53 +0900

    refactor: onesignal API 인자 수정

--- supabase/functions/onesignal/onesignalService.ts
+++ supabase/functions/onesignal/onesignalService.ts
@@ -15,28 +15,38 @@ export class OnesignalService {
     );
   }
 
-  async sendNotification(contents: {
+  async sendNotification(requestBody: {
     title: string;
     subtitle: string;
     message: string;
     data: unknown;
-    userIds: string[];
+    userIds?: string[];
+    segments?: string[];
   }) {
-    const requestBody = {
+    const baseProperties = {
       app_id: this.appId,
       android_channel_id: this.androidChannelId,
       target_channel: "push",
       small_icon: "ic_stat_onesignal_default",
       ios_badgeType: "Increase",
       ios_badgeCount: 1,
-      data: contents.data,
-      headings: { en: contents.title },
-      subtitle: { en: contents.subtitle },
-      contents: { en: contents.message },
-      include_external_user_ids: contents.userIds,
+      data: requestBody.data,
+      headings: { en: requestBody.title },
+      subtitle: { en: requestBody.subtitle },
+      contents: { en: requestBody.message },
     };
+    const additionalProperties = {
+      ...(requestBody.userIds &&
+        { include_external_user_ids: requestBody.userIds }),
+      ...(requestBody.segments && { included_segments: requestBody.segments }),
+    };
+    const onesignalRequestBody = {
+      ...baseProperties,
+      ...additionalProperties,
+    };
+
     const apiResponse = await this.onesignalClient.sendNotification(
-      requestBody,
+      onesignalRequestBody,
     );
     return apiResponse;
   }
